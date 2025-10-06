faec37340c7f2a38a2b7eb03d42fe5f546d42e1c
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-10-06 22:13:07 +0200

    Create JWT authentication with initial set() blocklist

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask
 from dotenv import load_dotenv
 
 from api.config import Config
 from api.extensions import api, jwt, db, migrate
-# from app.resources.test import blp as TestBlueprint
-# from app.resources.user import blp as UserBlueprint
-# from api import jwt_callbacks
+from api.resources.test import blp as TestBlueprint
+from api.resources.user import blp as UserBlueprint
+from api import jwt_callbacks
 
 def create_app():
     app = Flask(__name__)
@@ -20,7 +19,7 @@ def create_app():
     api.init_app(app)
     jwt.init_app(app)
 
-    # api.register_blueprint(TestBlueprint)
-    # api.register_blueprint(UserBlueprint)
+    api.register_blueprint(TestBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
