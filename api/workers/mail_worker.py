d571f1750bb3dab7421c2b635313088e42f98b09
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-10-13 22:16:58 +0200

    Add html template for registration email

--- api/workers/mail_worker.py
+++ api/workers/mail_worker.py
@@ -1,20 +1,30 @@
 import os
 import requests
+import jinja2
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("api/templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_mailgun_message(to, subject, body, html):
     return requests.post(
   	    f"https://api.mailgun.net/v3/{DOMAIN}/messages",
   		auth=("api", os.getenv("MAILGUN_API_KEY")),
   		data={"from": f"Mateusz Przybyla <postmaster@{DOMAIN}>",
 			"to": [to],
   			"subject": subject,
-  			"text": body})
+  			"text": body,
+            "html": html 
+        }
+    )
 
 def send_user_registration_email(email, username):
-    return send_simple_message(
+    return send_mailgun_message(
         to=email,  
         subject="Successfully signed up",
-        body=f"Hi {username}, you have successfully signed up for our service!"
+        body=f"Hi {username}, you have successfully signed up for our service!",
+        html=render_template("email/registration.html", username=username),
     )
