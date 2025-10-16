0522724997b7a22599c7206166b38d9a10419c9e
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-10-16 18:10:51 +0200

    Add tests configuration

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+
+from api import create_app
+from api.extensions import db
+from api.models import UserModel
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "DEBUG": False,
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+        "JWT_SECRET_KEY": "test-secret"
+    })
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def db_session(app):
+    with app.app_context():
+        yield db.session
